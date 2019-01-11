872eeea00ca8bf527e14be772bf5c6fe4e2c0747
Etienne Bertrand <etienne.bertrand@epitech.eu>
2019-01-11 15:57:27 +0100

    fix MyGpp

--- MyGpp/my_g++.sh
+++ MyGpp/my_g++.sh
@@ -1,6 +1,8 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 if g++ -Wall -Wextra $@
 then
    true
 else
-    ffplay -autoexit -nodisp boule_noire.mp3 &>/dev/null &
+    ffplay -autoexit -nodisp $DIR/boule_noire.mp3 &>/dev/null &
 fi
