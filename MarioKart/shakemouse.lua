543cbc9bb2b04a2f22339f61482b91c89a775326
Andgel <andgel.halley@epitech.eu>
2019-09-05 13:33:56 +0200

    Fixed shakemouse.lua

--- MarioKart/shakemouse.lua
+++ MarioKart/shakemouse.lua
@@ -5,9 +5,9 @@ local t = (args[1] == "inf" and 1 or args[1]) + args[2] + args[3]
 math.randomseed(tonumber(args[3]))
 
 function pos(max)
-    return math.random(0, max) - max / 2 - max % 2
+    return math.floor(math.random(0, max) - max / 2 - max % 2)
 end
 
 for i = 1, args[1] == "inf" and 1/0 or tonumber(args[1]) do
-    os.execute(("./xdotool mousemove_relative -- %i %i"):format(pos(max), pos(max)))
+    os.execute(("xdotool mousemove_relative -- %i %i"):format(pos(max), pos(max)))
 end
